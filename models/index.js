1f319cb34f8e820b858d068317bb3859b6798a71
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-19 15:50:17 -0500

    Added belongsTo and hasMany for User and Post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,19 @@
 const User = require('./User');
 const Post = require('./Post')
 
-module.exports = { User }, { Post };
+
+// Post belongsTo User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// User hasMany Post 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+
+module.exports = { 
+    User,
+    Post
+};
