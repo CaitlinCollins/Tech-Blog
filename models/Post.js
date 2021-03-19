8ef59a49f04a86abceae750ec4518b25b1ac1eda
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-19 15:31:54 -0500

    Created Post Model

--- models/Post.js
+++ models/Post.js
@@ -2,13 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
+class Post extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-User.init(
+Post.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -16,31 +16,29 @@ User.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        user_name: {
+        title: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        password: {
-            type: DataTypes.STRING,
+        content: {
+            type: DataTypes.TEXT,
             allowNull: false,
-            validate: {
-                len: [8],
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
             },
         },
     },
     {
-        hooks: {
-            beforeCreate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-        },
         sequelize,
-        timestamps: false,
+        timestamps: true,
         freezeTableName: true,
         underscored: true,
-        modelName: 'user',
+        modelName: 'post',
     }
 );
 
-module.exports = User;
+module.exports = Post;
