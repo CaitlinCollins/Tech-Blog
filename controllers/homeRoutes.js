0178145ce039534127c3021331ab7da35f7c2ae2
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-22 10:06:30 -0500

    Added withAuth to all api routes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,23 +2,49 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// direct user to login page on root and on /login routes
-router.get('/', async (req, res) => {
-  res.render('login');
+// Prevents access to the home page if not logged in.
+router.get('/', withAuth, async (req, res) => {
+  try {
+  res.render('home', {
+    logged_in: req.session.logged_in,
+  });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// Redirects to root if user is already logged in.
 router.get('/login', async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
-// direct user to dashboard on /dashboard route
-router.get('/dashboard', async (req, res) => {
-  res.render('dashboard');
+// Prevents access to the dashboard page if not logged in.
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    res.render('dashboard', {
+      logged_in: req.session.logged_in,
+    });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// direct user to home on /home route
-router.get('/home', async (req, res) => {
-  res.render('home');
+// Prevents access to the home page if not logged in.
+router.get('/home', withAuth, async (req, res) => {
+  try {
+  res.render('home', {
+    logged_in: req.session.logged_in,
+  });
+  }
+  catch (err){
+    res.status(500).json(err);
+  }
 });
 
 
