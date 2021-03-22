d88a69744de6b2ba4e5c7816a6c0a41220284006
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-21 19:53:21 -0500

    Created dashboard.handlebars for create post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 
+
+// direct user to login page on root and on /login routes
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  res.render('login');
 });
+
+router.get('/login', async (req, res) => {
+  res.render('login');
+});
+
+// direct user to dashboard on /dashboard route
+router.get('/dashboard', async (req, res) => {
+  res.render('dashboard');
+});
+
+// direct user to home on /home route
+router.get('/home', async (req, res) => {
+  res.render('home');
+});
+
+
 
 module.exports = router;
